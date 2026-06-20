7ef003a2744e7a19627dd8a781c6a7b1165269fa
shadyhegab2-svg <shadyhegab2@gmail.com>
2026-06-20 04:55:36 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,13 @@
-const CACHE='asad-v8';
-const ASSETS=['./', './index.html'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
-self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
-self.addEventListener('fetch',e=>{e.respondWith(fetch(e.request).then(res=>{if(res&&res.status===200&&e.request.method==='GET'){const clone=res.clone();caches.open(CACHE).then(c=>c.put(e.request,clone));}return res;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
-self.addEventListener('message',e=>{if(e.data==='skipWaiting')self.skipWaiting();});
+const CACHE='asad-v13';
+const FILES=['/asad-albahr/','/asad-albahr/index.html','/asad-albahr/manifest.json'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+});
